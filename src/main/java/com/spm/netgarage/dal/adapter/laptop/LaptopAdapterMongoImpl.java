d180bda35b6232758fa0ecb193be13d085b6e2af
paradocx96 <adilanga@gmail.com>
2021-08-17 21:32:57 +0530

    Laptop - Laptop Adapter MongoImpl Save Function

--- src/main/java/com/spm/netgarage/dal/adapter/laptop/LaptopAdapterMongoImpl.java
+++ src/main/java/com/spm/netgarage/dal/adapter/laptop/LaptopAdapterMongoImpl.java
@@ -1,5 +1,6 @@
 package com.spm.netgarage.dal.adapter.laptop;
 
+import com.spm.netgarage.dal.model.laptop.LaptopModel;
 import com.spm.netgarage.dal.repository.laptop.LaptopRepository;
 import com.spm.netgarage.domain.laptop.Laptop;
 import com.spm.netgarage.domain.laptop.LaptopDataAdapter;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -25,7 +27,52 @@ public class LaptopAdapterMongoImpl implements LaptopDataAdapter {
 
     @Override
     public Laptop save(Laptop laptop) {
-        return null;
+        LaptopModel laptopModel = new LaptopModel();
+
+        laptopModel.setUser(laptop.getUser());
+        laptopModel.setDatetime(LocalDateTime.now());
+        laptopModel.setStatus("Deactivate");
+        laptopModel.setName(laptop.getName());
+        laptopModel.setYear(laptop.getYear());
+        laptopModel.setBrand(laptop.getBrand());
+        laptopModel.setOs(laptop.getOs());
+        laptopModel.setModel(laptopModel.getModel());
+        laptopModel.setProcessorname(laptopModel.getProcessorname());
+        laptopModel.setProcessordetails(laptopModel.getProcessordetails());
+        laptopModel.setProcessorgeneration(laptopModel.getProcessorgeneration());
+        laptopModel.setChipset(laptopModel.getChipset());
+        laptopModel.setRamtype(laptopModel.getRamtype());
+        laptopModel.setRamcapacity(laptopModel.getRamcapacity());
+        laptopModel.setRamslotstype(laptopModel.getRamslotstype());
+        laptopModel.setRamslotscount(laptopModel.getRamslotscount());
+        laptopModel.setStoragefirst(laptopModel.getStoragefirst());
+        laptopModel.setStoragefirstcapacity(laptopModel.getStoragefirstcapacity());
+        laptopModel.setStoragesecond(laptopModel.getStoragesecond());
+        laptopModel.setStoragesecondcapacity(laptopModel.getStoragesecondcapacity());
+        laptopModel.setDisplaysizeresolution(laptopModel.getDisplaysizeresolution());
+        laptopModel.setDisplayrefreshrate(laptopModel.getDisplayrefreshrate());
+        laptopModel.setDisplaytype(laptopModel.getDisplaytype());
+        laptopModel.setGraphicbrand(laptopModel.getGraphicbrand());
+        laptopModel.setGraphicmodel(laptopModel.getGraphicmodel());
+        laptopModel.setGraphiccapacity(laptopModel.getGraphiccapacity());
+        laptopModel.setGraphicdetails(laptopModel.getGraphicdetails());
+        laptopModel.setWebcam(laptopModel.getWebcam());
+        laptopModel.setKeyboard(laptopModel.getKeyboard());
+        laptopModel.setCommunication(laptopModel.getCommunication());
+        laptopModel.setAudio(laptopModel.getAudio());
+        laptopModel.setIoports(laptopModel.getIoports());
+        laptopModel.setBattery(laptopModel.getBattery());
+        laptopModel.setDimension(laptopModel.getDimension());
+        laptopModel.setWeight(laptopModel.getWeight());
+        laptopModel.setColor(laptopModel.getColor());
+
+        // Saving model data in database
+        laptopModel = repository.save(laptopModel);
+
+        // Assign auto generated key to object and return object
+        laptop.setId(laptopModel.getId());
+
+        return laptop;
     }
 
     @Override
